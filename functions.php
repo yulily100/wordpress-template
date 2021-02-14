0ddae8f7db71012b61c32124b9d26eed3e7923e5
yulily100 <yulily100@users.noreply.github.com>
2021-02-14 23:35:47 +0900

    css書き換え

--- functions.php
+++ functions.php
@@ -1,82 +1,60 @@
 <?php
-// カスタム投稿タイプを定義
-	function register_member() {
-		$labels = array(
-			'name' => __( 'メンバー', 'member' ),
-			'singular_name' => __( 'メンバー', 'member' ),
-			'add_new' => __( '新規作成', 'member' ),
-			'add_new_item' => __( '新しいメンバープロフィールを追加', 'member' ),
-			'edit_item' => __( '編集', 'member' ),
-			'new_item' => __( '新しいメンバー', 'member' ),
-			'view_item' => __( 'メンバープロフィールを見る', 'member' ),
-			'search_items' => __( '検索', 'member' ),
-			'not_found' => __( '見つかりません', 'member' ),
-			'not_found_in_trash' => __( 'ゴミ箱にはありません', 'member' ),
-			'parent_item_colon' => __( '親メンバー', 'member' ),
-			'menu_name' => __( 'メンバー', 'member' ),
-		);
-
-		$args = array(
-			'labels' => $labels,
-			'hierarchical' => false,
-			'supports' => array( 'title', 'thumbnail', 'custom-fields' ),
-
-			'public' => true,
-			'menu_position' => 5,
-			'show_ui' => true,
-			'show_in_menu' => true,
-
-			'show_in_nav_menus' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'has_archive' => true,
-			'query_var' => true,
-			'can_export' => true,
-			'rewrite' => true,
-			'capability_type' => 'post'
-		);
-		register_post_type( 'member', $args );
-
-		// カテゴリー追加
-		$category = array(
-			'label' => 'ジャンル',
-			'public' => true,
-			'hierarchical' => true
-		);
-		register_taxonomy('genre', 'member', $category);
-	}
-	add_action( 'init', 'register_member' );
-
-
 	//アイキャッチ画像を使用する
 	add_theme_support('post-thumbnails');
 
-	//サイズを指定して切り抜きをする(縦：100px 横100px)
-	set_post_thumbnail_size(300, 300, true);
-
 	// entry page
-	add_image_size('entry-image',800, 400, true);
+	add_image_size('entry-top',740, 416, true);
 
-	// member page
-	add_image_size('member-image',300, 300, true);
+	// title
+	add_theme_support( 'title-tag' );
 
-	function redirect_404_to_home() {
-	  if (is_404()) {
-	    wp_safe_redirect(home_url('/'), 302);
-	    exit();
-	  }
+	// カスタム投稿タイプ
+	function register_plan () {
+		$args = array(
+			'label' => 'プラン',
+			'labels' => array(
+				'name' => 'プラン一覧',
+				'add_new' => '新しいプランを追加',
+				'add_new_item' => '新しいプランを追加',
+			),
+			'public' => true,
+			'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields'),
+			'menu_position' => 5,
+			'menu_icon'   => 'dashicons-welcome-edit-page',
+			'has_archive' => 'true',
+			'rewrite' => array('with_front' => false), // パーマリンク書き換え、設定空更新必要
+		);
+		register_post_type( "plan", $args);
+
+		$tags = array(
+			'label' => 'タグ',
+			'public' => true,
+		);
+		register_taxonomy( 'plan_tags', 'plan', $tags);
+
+		$categories = array(
+			'label' => 'カテゴリー',
+			'public' => true,
+		);
+		register_taxonomy( 'plan_categories', 'plan', $categories);
 	}
-	add_action('template_redirect', 'redirect_404_to_home');
+	add_action('init', 'register_plan');
 
-	function defer_script($tag) {
-		return str_replace("type='text/javascript'", "defer", $tag);
+
+
+	// ダッシュボードのメニューをカスタマイズ
+	function remove_menus () {
+		global $menu;
+		unset($menu[25]); // コメント
 	}
-	add_filter('script_loader_tag','defer_script');
+	add_action('admin_menu', 'remove_menus');
 
-	// JSのロード
-	wp_register_script('three', get_template_directory_uri() . '/js/three.min.js', array(), false, true);
-	wp_register_script('three-postprocessing', get_template_directory_uri() . '/js/postprocessing.min.js', array('three'), false, true);
-	wp_register_script('scrollreveal', 'https://unpkg.com/scrollreveal/dist/scrollreveal.min.js', array(), false, true);
-	wp_register_script('index', get_template_directory_uri() . '/yokoito.js', array('three', 'three-postprocessing', 'scrollreveal'), false, true);
-	wp_enqueue_script('index');
+	// 日本語URL禁止
+	function auto_post_slug( $slug, $post_ID, $post_status, $post_type ) {
+    if ( preg_match( '/(%[0-9a-f]{2})+/', $slug ) ) {
+        $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID;
+    }
+    return $slug;
+	}
+	add_filter( 'wp_unique_post_slug', 'auto_post_slug', 10, 4  );
 ?>
--- header.php
+++ header.php
@@ -17,8 +17,5 @@
 
   <body <?php body_class(); ?>>
     <header class="header">
-      <div class="header-inner">
-        <!-- <h1 class="header-logo"><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="tsubottle"></a></h1> -->
-        <h1>tsubottle</h1>
-      </div>
+        <h1>title</h1>
     </header>
